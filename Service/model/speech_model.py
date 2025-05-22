edb0407034c249bd6b5d9d7a691af961bf8543b1
Muhammad Ahmed Abbasi <engrahmedabbasi3015@gmail.com>
2025-05-22 16:12:49 +0800

    new

--- Service/model/speech_model.py
+++ Service/model/speech_model.py
@@ -1,32 +1,35 @@
-# from funasr import AutoModel
+from funasr import AutoModel
+import os
 
-
-# class SpeechRecognitionModel:
-#     def __init__(self):
-#         self.model = None
-#         self.load_model()
+class SpeechRecognitionModel:
+    def __init__(self):
+        self.model = None
+        self.load_model()
     
-#     def load_model(self):
-#         """Initialize the ASR model with specified configurations"""
-#         self.model = AutoModel(
-#             model=r"D:\Backend_Algorithm_travel_assistance\downloaded_models\paraformer-zh", 
-#             model_revision="v2.0.4",
-#             vad_model=r"D:\Backend_Algorithm_travel_assistance\downloaded_models\fsmn-vad", 
-#             vad_model_revision="v2.0.4",
-#             punc_model=r"D:\Backend_Algorithm_travel_assistance\downloaded_models\ct-punc", 
-#             punc_model_revision="v2.0.4"
-#         )
+    def load_model(self):
+        """Initialize the ASR model with specified configurations using relative paths"""
+        base_dir = os.path.dirname(os.path.abspath(__file__))
+        models_dir = os.path.abspath(os.path.join(base_dir, '..', '..', 'downloaded_models'))
 
-#     def transcribe(self, audio_data):
-#         """Transcribe audio data to text"""
-#         res = self.model.generate(
-#             input=audio_data,
-#             cache={},
-#             language="zn",
-#             use_itn=True,
-#             batch_size_s=60,
-#             merge_vad=True,
-#             merge_length_s=15,
-#         )
-#         return res[0]["text"] if res else ""
+        self.model = AutoModel(
+            model=os.path.join(models_dir, 'paraformer-zh'), 
+            model_revision="v2.0.4",
+            vad_model=os.path.join(models_dir, 'fsmn-vad'), 
+            vad_model_revision="v2.0.4",
+            punc_model=os.path.join(models_dir, 'ct-punc'), 
+            punc_model_revision="v2.0.4"
+        )
+
+    def transcribe(self, audio_data):
+        """Transcribe audio data to text"""
+        res = self.model.generate(
+            input=audio_data,
+            cache={},
+            language="zn",
+            use_itn=True,
+            batch_size_s=60,
+            merge_vad=True,
+            merge_length_s=15,
+        )
+        return res[0]["text"] if res else ""
     
